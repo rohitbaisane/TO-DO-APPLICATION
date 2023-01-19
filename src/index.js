0914c0d039ba95b0546656292e9731e9c21c0c55
rohitbaisane <rohitbaisane91@gmail.com>
2023-01-19 21:30:17 +0530

    Create basic setup for project

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+const { PORT } = require('./config/serverConfig');
+const prepareAndStartServer = () => {
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.listen(PORT, () => {
+        console.log('server started listening');
+    });
+}
+
+prepareAndStartServer();
